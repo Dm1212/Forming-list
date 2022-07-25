95e0d8914d1c3f19de68e52aa6b1c1d159645a4d
Dm1212 <primakov.dm@yandex.ru>
2022-07-25 14:32:56 +0500

    'End'

--- first.py
+++ first.py
@@ -1,20 +1,30 @@
 x = int(input('Сколько списков будет? : '))
 y = 1
 w = 0	
-l = []
+list = []
+sort_list = []
 
 for i in range(x):
-	l.append([])
+	list.append([])
 
 while y <= x:
-	print('Введите значение которое будем добавлять в список', y, ': ')	
+	print('Введите значение которое будем добавлять в список №', y, 'либо Нажмите Enter, если список полон:\n')	
 	value = input()
 	if value == '':
+		print('Формирование списка', y, 'закончено\n')
 		w += 1
 		y += 1
+		if y <= x:
+			print('Формируем спиок', y, '\n')
 		continue
-	print(value, 'Добавлено в список №', y)
-	l[w].append(value)
-	
+	list[w].append(int(value))
 
-print(l)
+def uniq_list(x, y):
+	for i in x:
+		for n in i:
+			if n not in y:
+				y.append(n)
+	y.sort()
+	return(y)
+
+print(uniq_list(list, sort_list))
